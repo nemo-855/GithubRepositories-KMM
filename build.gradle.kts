d80b390583dee4de40a85632360b3a69374816a1
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2023-03-23 16:05:35 +0900

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.1")
+    }
+}
+plugins {
+    //trick: for the same plugin versions in all sub-modules
+    id("com.android.library").version("7.4.2").apply(false)
+    kotlin("multiplatform").version("1.8.0").apply(false)
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
