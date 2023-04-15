cbbfbc18b9a604661f876a68611c77de9376fa27
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2023-04-15 10:11:12 +0900

    fix: return flow at usecase layer

--- shared/src/commonMain/kotlin/com/nemo/githubrepositories_kmm/domain/GithubUseCase.kt
+++ shared/src/commonMain/kotlin/com/nemo/githubrepositories_kmm/domain/GithubUseCase.kt
@@ -1,7 +1,8 @@
 package com.nemo.githubrepositories_kmm.domain
 
 import com.nemo.githubrepositories_kmm.data.models.GithubProject
+import kotlinx.coroutines.flow.Flow
 
 interface GithubUseCase {
-    suspend fun fetchGithubProjects(userName: String): List<GithubProject>
+    suspend fun fetchGithubProjects(userName: String): Flow<List<GithubProject>>
 }
--- shared/src/commonMain/kotlin/com/nemo/githubrepositories_kmm/domain/GithubUseCaseImpl.kt
+++ shared/src/commonMain/kotlin/com/nemo/githubrepositories_kmm/domain/GithubUseCaseImpl.kt
@@ -2,11 +2,15 @@ package com.nemo.githubrepositories_kmm.domain
 
 import com.nemo.githubrepositories_kmm.data.models.GithubProject
 import com.nemo.githubrepositories_kmm.data.repositories.interfaces.GithubRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 internal class GithubUseCaseImpl(
     private val githubRepository: GithubRepository
 ) : GithubUseCase {
-    override suspend fun fetchGithubProjects(userName: String): List<GithubProject> {
-        return githubRepository.fetchGithubProjects(userName = userName)
+    override suspend fun fetchGithubProjects(userName: String): Flow<List<GithubProject>> {
+        return flow {
+            githubRepository.fetchGithubProjects(userName = userName)
+        }
     }
 }
